d84fc2f9c45617fd804a4c8b0b6bca5d9b21f5ae
ranjanmangla1 <ranjanmangla1@gmail.com>
2024-02-05 14:14:54 +0000

    some more fixes to initial project setup

---
+++ backend_logic/python/src/auth/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+import jwt, datetime, os
+
+server = flask(__name__)
+mysql = MySQL(server)
+
+# config
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
