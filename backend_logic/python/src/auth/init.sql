f6905902c0845178ffab7e341634f334e31da9cc
ranjanmangla1 <ranjanmangla1@gmail.com>
2024-02-07 01:40:20 +0530

    set up the project, fixed docker file & tagged image on hub, and local set up is ready
    Signed-off-by: ranjanmangla1 <ranjanmangla1@gmail.com>

--- backend_logic/python/src/auth/init.sql
+++ backend_logic/python/src/auth/init.sql
@@ -2,15 +2,15 @@ CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
 
 CREATE DATABASE AUTH;
 
-GRANT ALL PRIVILIGES
+GRANT ALL PRIVILEGES
 ON auth.* TO 'auth_user'@'localhost';
 
-USE auth;
+USE AUTH;
 
 CREATE TABLE user (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
-  pasword VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL
 );
 
 INSERT INTO user (email, password)
